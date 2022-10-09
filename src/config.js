4cd2a7cdffe4f4c9eff3954a41ec2f379c41c406
ARCANE <malayp003@gmail.com>
2022-10-09 22:41:09 +0530

    client

--- src/config.js
+++ src/config.js
@@ -3,9 +3,8 @@ const configs = {
       SERVER_URI: 'http://localhost:3001',
     },
     production: {
-      SERVER_URI: '/api',
+      SERVER_URI: 'https://udhari.herokuapp.com',
     },
   };
   
-console.log(process.env.NODE_ENV);
 module.exports.config = configs[process.env.NODE_ENV];
